75e360923f81158dc0400e147b42ecf2a53d16a6
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-01-18 15:23:09 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # sample value for your variables
 #MYVARVALUE="nginx:latest"
 IMAGE='test'
-BUILD_ID=6
+BUILD_ID='6'
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
 template=`cat "deployment.yml.template" | sed -e "s/{{IMAGE}}/$IMAGE/g" -e "s/{{BUILD_ID}}/$BUILD_ID/g"`
