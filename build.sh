864739935796bfd10952ddfc632fe818e0849533
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-01-18 15:14:20 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 # sample value for your variables
 #MYVARVALUE="nginx:latest"
 IMAGE='test'
+BUILD_ID=6
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
-template=`cat "deployment.yml.template" | sed "s/{{IMAGE}}/$IMAGE/g"`
+template=`cat "deployment.yml.template" | sed "s/{{IMAGE}}/{{BUILD_ID}},/$IMAGE/$BUILD_ID/g"`
 
 # apply the yml with the substituted value
 echo "$template" | kubectl apply -f - --kubeconfig=/home/manhnh/.kube/config
